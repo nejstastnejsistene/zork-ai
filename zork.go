94771b79de7f58674f5055ea54f1fbe1e6e552a3
Peter Johnson <peter@peterjohnson.io>
2015-04-19 22:38:54 -0400

    major refactoring to make the wrapper as smooth as possible
    The function to read stuff doesn't look for a sentinal but instead
    reads until no data has been received for a certain amount of time.
    This means it works properly for everything zork can throw at us,
    even if it doesn't end in a '>' prompt. Also the main loop was
    restructured so that commands can be silently executed without the
    user knowing (demonstrated as an auto save currently).

--- zork.go
+++ zork.go
@@ -1,69 +1,175 @@
 package main
 
 import (
-	"bufio"
+	"crypto/rand"
+	"encoding/hex"
+	"errors"
 	"fmt"
 	"io"
 	"log"
 	"os"
 	"os/exec"
 	"strings"
+	"sync"
+	"syscall"
+	"time"
 )
 
+// How long of a timeout should indicate the end of input.
+const timeout = 10 * time.Millisecond
+
 type Zork struct {
-	Cmd    *exec.Cmd
-	Stdin  io.WriteCloser
-	Stdout io.ReadCloser
+	Cmd        *exec.Cmd   // The zork process.
+	Exit       chan error  // Channel with zork's exit status.
+	Error      chan error  // Channel for our errors.
+	ZorkInput  io.Writer   // Zork's stdin.
+	ZorkOutput chan string // Stream of output from
+	Stdin      chan string // Stream of input from stdin.
+	Mutex      sync.Mutex  // Mutex to protect
 }
 
-func NewZork(dfrotz, zork1Dat string) (z *Zork, err error) {
-	z = new(Zork)
+func RunZork(dfrotz, zork1Dat string) (err error) {
+	z := new(Zork)
+	z.Error = make(chan error, 2)
 	z.Cmd = exec.Command(dfrotz, zork1Dat)
-	z.Stdin, err = z.Cmd.StdinPipe()
+	// Pipes to process.
+	z.ZorkInput, err = z.Cmd.StdinPipe()
 	if err != nil {
 		return
 	}
-	z.Stdout, err = z.Cmd.StdoutPipe()
+	var stdout io.ReadCloser
+	stdout, err = z.Cmd.StdoutPipe()
 	if err != nil {
 		return
 	}
-	return
-}
-
-func (z *Zork) Run() error {
+	// Start process.
 	if err := z.Cmd.Start(); err != nil {
 		log.Fatal(err)
 	}
-	done := make(chan error, 1)
+	// Wait for process to exit.
+	z.Exit = make(chan error, 1)
 	go func() {
-		done <- z.Cmd.Wait()
+		z.Exit <- z.Cmd.Wait()
 	}()
+	// Kill the process when finished if it isn't already dead
+	defer func() {
+		// Signal 0 checks if the process is alive.
+		if z.Cmd.Process.Signal(syscall.Signal(0)) == nil {
+			z.Cmd.Process.Kill()
+		}
+	}()
+	// Channels to read data from zork's stdout and our stdin.
+	z.ZorkOutput = sepByTimeout(stdout, timeout)
+	z.Stdin = sepByTimeout(os.Stdin, timeout)
 
-	input := readFrom(os.Stdin, '\n')
-	output := readFrom(z.Stdout, '>')
+	// Handle initial output.
+	z.HandleAsync("", <-z.ZorkOutput)
+	// Main loop.
 	for {
+		// Read input from stdin (excepting errors).
 		select {
-		case s := <-input:
-			z.HandleInput(s)
-		case s := <-output:
-			z.HandleOutput(s)
-		case err := <-done:
+		case input := <-z.Stdin:
+			// Pipe the input to zork and read the output.
+			input = strings.TrimSpace(input)
+			output, err := z.EvaluateCommand(input)
+			if err != nil {
+				return err
+			}
+			// Process the input/output pair.
+			go z.Handle(input, output)
+		case err := <-z.Exit:
+			return err
+		case err := <-z.Error:
 			return err
 		}
 	}
 }
 
-func (z *Zork) Input(s string) {
-	z.Stdin.Write([]byte(s + "\n"))
+// Returns a channel that yields all data received by a reader only after
+// a timeout is reached. Needed for zork because there is never an EOF, and you
+// ordinarly only know the end of input by visually seeing that no more input
+// is coming.
+func sepByTimeout(r io.Reader, timeout time.Duration) chan string {
+	// Continuosly read chunks.
+	chunks := make(chan []byte)
+	go func() {
+		chunk := make([]byte, 4096)
+		for {
+			n, err := r.Read(chunk)
+			// End input on error or if nothing is read.
+			if n == 0 || err != nil {
+				close(chunks)
+				return
+			}
+			chunks <- chunk[:n]
+		}
+	}()
+	// Accumulate chunks until the timeout is reached.
+	result := make(chan string)
+	var buf []byte
+	go func() {
+		for {
+			select {
+			// Another chunk, timeout not reached yet.
+			case chunk, ok := <-chunks:
+				// End of input.
+				if !ok {
+					close(result)
+					return
+				}
+				buf = append(buf, chunk...)
+			// Timeout occurs before next data, yield accumulated buffer.
+			case <-time.After(timeout):
+				if len(buf) > 0 {
+					result <- string(buf)
+					buf = nil
+				}
+			}
+		}
+	}()
+	return result
 }
 
-func (z *Zork) HandleInput(s string) {
-	s = strings.TrimSpace(s)
-	z.Input(s)
+// Input a command (excluding newline) into zork and return its output.
+func (z *Zork) EvaluateCommand(command string) (output string, err error) {
+	// Write the input (plus a newline) to zork.
+	if _, err = z.ZorkInput.Write([]byte(command + "\n")); err != nil {
+		return
+	}
+	// Read the output from zork, excepting the process dying.
+	select {
+	case output = <-z.ZorkOutput:
+		return
+	case err = <-z.Exit:
+		return
+	case err = <-z.Error:
+		return
+	}
 }
 
-func (z *Zork) HandleOutput(s string) {
-	lines := strings.Split(s, "\n")
+func (z *Zork) HandleAsync(input, output string) {
+	if err := z.Handle(input, output); err != nil {
+		z.Error <- err
+	}
+}
+
+func (z *Zork) Handle(input, output string) (err error) {
+	// Only let this run once at a time.
+	z.Mutex.Lock()
+	defer z.Mutex.Unlock()
+	// Print the output like it usually would.
+	fmt.Print(output)
+	os.Stdout.Sync()
+
+	// Save to a random file, as demo of what can be done!
+	b := make([]byte, 8)
+	rand.Read(b)
+	name := hex.EncodeToString(b)
+	if err = z.Save(name+".sav", false); err != nil {
+		return
+	}
+
+	/*lines := strings.Split(output, "\n")
 	// Determine if there is a header or not If there is not a header,
 	// than a move was not completed.
 	headerFields := strings.Fields(lines[0])
@@ -80,34 +186,40 @@ func (z *Zork) HandleOutput(s string) {
 	lines = lines[:len(lines)-2]
 	if lines[0] == locationName {
 		// lines now contains the description
-	}
-	fmt.Print(s)
+	}*/
+	return
 }
 
-func readFrom(r io.ReadCloser, sentinal byte) chan string {
-	ch := make(chan string)
-	go func() {
-		reader := bufio.NewReader(r)
-		for {
-			chunk, err := reader.ReadString(sentinal)
-			if err == nil {
-				ch <- chunk
-			}
+func (z *Zork) Save(path string, overwrite bool) (err error) {
+	var output string
+	_, err = z.EvaluateCommand("save")
+	if err != nil {
+		return nil
+	}
+	output, err = z.EvaluateCommand(path)
+	if err != nil {
+		return
+	}
+	if output == "Overwrite existing file? " {
+		if !overwrite {
+			return errors.New("save file already exists")
 		}
-	}()
-	return ch
+		output, _ = z.EvaluateCommand("y")
+		if err != nil {
+			return
+		}
+	}
+	if !strings.HasPrefix(output, "Ok") {
+		err = errors.New(output)
+	}
+	return
 }
 
 func main() {
 	if len(os.Args) != 3 {
-		fmt.Fprintf(os.Stderr, "usage: %s <dfrotz> <ZORK1.DAT>\n", os.Args[0])
-		os.Exit(2)
+		log.Fatalf("usage: %s <dfrotz> <ZORK1.DAT>\n", os.Args[0])
 	}
-	z, err := NewZork(os.Args[1], os.Args[2])
-	if err != nil {
-		log.Fatal(err)
-	}
-	if err = z.Run(); err != nil {
+	if err := RunZork(os.Args[1], os.Args[2]); err != nil {
 		log.Fatal(err)
 	}
 }
